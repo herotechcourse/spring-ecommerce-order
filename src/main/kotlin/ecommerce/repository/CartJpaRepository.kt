709a12e3380bc826f98c0596285cdd2eabf63687
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-08-05 16:46:28 +0200

    refactor: remove @Repository from interfaces extending JpaRepository

--- src/main/kotlin/ecommerce/repository/CartJpaRepository.kt
+++ src/main/kotlin/ecommerce/repository/CartJpaRepository.kt
@@ -4,9 +4,7 @@ import ecommerce.entity.Cart
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface CartJpaRepository : JpaRepository<Cart, Long> {
     fun findByMemberId(memberId: Long): List<Cart>
 
--- src/main/kotlin/ecommerce/repository/ProductJpaRepository.kt
+++ src/main/kotlin/ecommerce/repository/ProductJpaRepository.kt
@@ -4,9 +4,7 @@ import ecommerce.entity.Product
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface ProductJpaRepository : JpaRepository<Product, Long> {
     fun existsByName(name: String): Boolean
 
