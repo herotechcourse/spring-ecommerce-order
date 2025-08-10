82d87802d75d416d790f329489074c22180f80bd
pebencze <petra.bencze@deliveryhero.com>
2025-08-10 13:21:22 +0200

    feat( PaymentService.kt ): blueprint for PaymentService

---
+++ src/main/kotlin/ecommerce/service/PaymentService.kt
@@ -0,0 +1,30 @@
+package ecommerce.service
+
+import ecommerce.client.StripeClient
+import ecommerce.dto.OrderPlacementResponse
+import ecommerce.dto.PaymentRequest
+import ecommerce.exception.BadRequestException
+import ecommerce.exception.ExternalServiceException
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+@Transactional
+class PaymentService(
+    private val stripeClient: StripeClient
+) {
+    fun createPaymentIntent(req: PaymentRequest): OrderPlacementResponse {
+        try {
+            val response = stripeClient.createCheckoutSession(req)
+            return StringToOrderPlacementResponse(response)
+        } catch (e: BadRequestException) {
+            throw e
+        } catch (e: ExternalServiceException) {
+            throw e
+        } catch (e: Exception) {
+            // retry??
+            throw RuntimeException(e) // this should roll back the transaction
+        }
+    }
+
+}
