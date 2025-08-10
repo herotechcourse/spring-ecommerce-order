4a18b1aca633d351c79e0727a00ecfd92e12d22b
pebencze <petra.bencze@deliveryhero.com>
2025-08-10 18:45:48 +0200

    feat( OrderController.kt ): blueprint

---
+++ src/main/kotlin/ecommerce/controller/OrderController.kt
@@ -0,0 +1,33 @@
+package ecommerce.controller
+
+import ecommerce.annotation.LoginMember
+import ecommerce.dto.OrderDto
+import ecommerce.dto.OrderPlacementRequest
+import ecommerce.dto.OrderPlacementResponse
+import ecommerce.dto.RegisteredMember
+import ecommerce.service.OrderService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OrderController(
+    private val orderService: OrderService
+) {
+    @PostMapping("/orders/place")
+    fun placeOrder(
+        @Valid @RequestBody req: OrderPlacementRequest,
+        @LoginMember member: RegisteredMember
+    ): OrderPlacementResponse {
+        val response = orderService.placeOrder(req, member)
+        return response
+    }
+
+//    @GetMapping("/orders")
+//    fun getOrders(@LoginMember member: RegisteredMember): List<OrderDto> {
+//        val orders = orderService.getAllOrdersForMember()
+//        return orders
+//    }
+}
