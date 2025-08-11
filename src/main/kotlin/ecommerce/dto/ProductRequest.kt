e11df0beaad39c97e3bff8b2e93081ba6f5cf1ad
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-08-11 13:04:30 +0200

    refactor(product): remove validation from DTO, change price type to int

--- src/main/kotlin/ecommerce/dto/ProductRequest.kt
+++ src/main/kotlin/ecommerce/dto/ProductRequest.kt
@@ -8,21 +8,11 @@ import jakarta.validation.constraints.Size
 
 data class ProductRequest(
     @field:NotBlank(message = "Name must not be blank")
-    @field:Size(max = 15, message = "Name must be 15 characters or fewer")
-    @field:Pattern(
-        regexp = """^[\w\s\[\]\(\)\+\-\&/_]+$""",
-        message = "Name contains invalid characters",
-    )
     val name: String,
     @field:Min(value = 1, message = "Price must be greater than 0")
-    val price: Double,
+    val price: Int,
     @field:NotBlank(message = "Image URL must not be blank")
-    @field:Pattern(
-        regexp = """^https?://.*""",
-        message = "Image URL must start with http:// or https://",
-    )
     val imageUrl: String,
     @field:Size(min = 1, message = "At least one option is required")
-    @field:Valid
     val options: List<OptionRequest>,
 )
