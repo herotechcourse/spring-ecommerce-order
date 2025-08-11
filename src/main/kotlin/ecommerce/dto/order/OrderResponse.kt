472108344bc234bf3f61b24a462c8adbdd2c2d0a
saragrosser <sgrosser00@gmail.com>
2025-08-11 13:00:26 +0200

    feat(order): add Order DTOs

---
+++ src/main/kotlin/ecommerce/dto/order/OrderResponse.kt
@@ -0,0 +1,15 @@
+package ecommerce.dto.order
+
+import ecommerce.entity.OrderStatus
+import java.time.LocalDateTime
+
+data class OrderResponse(
+    val id: Long,
+    val orderedAt: LocalDateTime,
+    val status: OrderStatus,
+    val totalAmount: Long,
+    val currency: String,
+    val paymentIntentId: String?,
+    val paymentStatus: String?,
+    val items: List<OrderItemResponse>
+)
