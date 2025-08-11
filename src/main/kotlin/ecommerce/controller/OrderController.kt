cb3879a1c07cdbedd11b7a4e4056ad1e8bfa26f7
pebencze <petra.bencze@deliveryhero.com>
2025-08-11 09:11:27 +0200

    style( OrderController.kt )

--- src/main/kotlin/ecommerce/controller/OrderController.kt
+++ src/main/kotlin/ecommerce/controller/OrderController.kt
@@ -10,13 +10,15 @@ import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/orders")
 class OrderController(
     private val orderService: OrderService,
 ) {
-    @PostMapping("/orders/place")
+    @PostMapping("/place")
     fun placeOrder(
         @Valid @RequestBody req: OrderPlacementRequest,
         @LoginMember member: RegisteredMember,
@@ -25,7 +27,7 @@ class OrderController(
         return response
     }
 
-    @GetMapping("/orders")
+    @GetMapping
     fun getOrders(
         @LoginMember member: RegisteredMember,
     ): List<OrderDto> {
