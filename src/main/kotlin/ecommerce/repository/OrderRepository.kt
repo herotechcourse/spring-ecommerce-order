d33bd085b61fede6ff8505e97d3cbe4c037777bf
knopgm <knop.gm@gmail.com>
2025-08-18 23:56:36 +0200

    feat(repository): add findByIdAndMemberId method to OrderRepository for member-specific order retrieval

--- src/main/kotlin/ecommerce/repository/OrderRepository.kt
+++ src/main/kotlin/ecommerce/repository/OrderRepository.kt
@@ -3,4 +3,9 @@ package ecommerce.repository
 import ecommerce.entity.Order
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long> {
+    fun findByIdAndMemberId(
+        orderId: Long,
+        memberId: Long,
+    ): Order?
+}
