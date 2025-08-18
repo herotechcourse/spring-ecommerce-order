d606b36f927f4f45a4473779e6e16811eb425ad3
knopgm <knop.gm@gmail.com>
2025-08-18 23:56:57 +0200

    feat(service): improve createPaymentIntent method to include paymentMethod parameter and automatic payment methods configuration

--- src/main/kotlin/ecommerce/service/payment/StripeClientService.kt
+++ src/main/kotlin/ecommerce/service/payment/StripeClientService.kt
@@ -9,16 +9,30 @@ class StripeClientService {
     fun createPaymentIntent(
         amount: Long,
         currency: String,
+        paymentMethod: String,
     ): PaymentIntent {
         val params =
             PaymentIntentCreateParams.builder()
                 .setAmount(amount)
                 .setCurrency(currency)
+                .setPaymentMethod(paymentMethod)
+                .setAutomaticPaymentMethods(
+                    PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
+                        .setEnabled(true)
+                        .setAllowRedirects(PaymentIntentCreateParams.AutomaticPaymentMethods.AllowRedirects.NEVER)
+                        .build(),
+                )
                 .build()
+
         return PaymentIntent.create(params)
     }
 
     fun retrievePaymentIntent(id: String): PaymentIntent {
         return PaymentIntent.retrieve(id)
+    }
+
+    fun confirmPaymentIntent(id: String) {
+        val paymentIntent = retrievePaymentIntent(id)
+        paymentIntent.confirm()
     }
 }
