21aec2fd4dd49dec6d17d45508c962150c2a5750
pebencze <petra.bencze@deliveryhero.com>
2025-08-26 14:40:46 +0200

    fix( build.gradle.kts ): remove stripe library

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.stripe:stripe-java:29.4.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
 
