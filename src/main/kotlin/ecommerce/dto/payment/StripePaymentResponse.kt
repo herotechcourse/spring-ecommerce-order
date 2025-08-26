7d3db90a62b9159e1fa0daf3fe21b0eb65a0b821
saragrosser <sgrosser00@gmail.com>
2025-08-27 01:19:03 +0200

    test(client): Replace integration test with unit test for StripeClient -Allow RestClient injection for testability

--- src/main/kotlin/ecommerce/dto/payment/StripePaymentResponse.kt
+++ src/main/kotlin/ecommerce/dto/payment/StripePaymentResponse.kt
@@ -15,3 +15,17 @@ data class StripeErrorDetails(
     val code: String?,
     val message: String?,
 )
+
+data class StripePaymentIntentRequest(
+    val amount: Long,
+    val currency: String = "usd",
+    val confirm: Boolean = true,
+    @get:JsonProperty("automatic_payment_methods[enabled]")
+    val automaticPaymentMethodsEnabled: Boolean = true,
+    @get:JsonProperty("automatic_payment_methods[allow_redirects]")
+    val automaticPaymentMethodsAllowRedirects: String = "never",
+)
+
+data class StripeErrorResponse(
+    val error: StripeErrorDetails,
+)
