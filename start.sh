0fe65fb271fc23ce1c62a5235edad505bcad4782
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-08-26 12:33:00 +0200

    update start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JAR_NAME="spring-ecommerce-0.0.1-SNAPSHOT.jar"
-DEPLOY_PATH="/home/ubuntu/app/"
+DEPLOY_PATH="/home/ubuntu/spring-ecommerce-order/build/libs/"
 
 echo "> Checking for a currently running application..."
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -15,6 +15,8 @@ else
   sleep 5
 fi
 
+./gradlew bootJar
+
 echo "> Starting application..."
-export $(grep -v '^#' $DEPLOY_PATH/.env | xargs)
+export $(grep -v '^#' "/home/ubuntu/spring-ecommerce-order/.env" | xargs)
 nohup java -jar $DEPLOY_PATH$JAR_NAME > ./application.log 2>&1 &
